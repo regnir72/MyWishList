c83ddf318f92a69b05fa64370ecefb5eb4e53c2c
Dell <regnir72@gmail.com>
2023-03-26 08:34:50 +0200

    WishInfo methods

--- src/main/java/com/gyp/mywishlist/WishInfo.java
+++ src/main/java/com/gyp/mywishlist/WishInfo.java
@@ -1,8 +1,32 @@
 package com.gyp.mywishlist;
 
+import java.util.ArrayList;
+
 public class WishInfo
 {
+    com.gyp.mywishlist.Wish wish;
+    ArrayList<com.gyp.mywishlist.Wish> wishes = new ArrayList<>();
  
+    public void newWish(String name, int price)
+    {
+        
+    }    
+    public void removeWish()
+    {
+        
+    }    
+    public int numberOfWishes()
+    {
+        return 0;
+    }
+    public int totalValueOfWishes()
+    {
+        return 0;
+    }
+    public boolean isBuyable()
+    {
+        return false;
+    }
     
     
     public class Wish
